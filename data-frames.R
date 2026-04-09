c33d06081fb19a297592ec53febfa7f53f48c866
Immaculate Munde <immaculatemunde1@gmail.com>
2026-04-09 13:04:30 +0300

    Numerical and graphical operations

--- data-frames.R
+++ data-frames.R
@@ -5,4 +5,14 @@ df<-data.frame(
   Gender = c("Female","Male","Male"),
   Score = c(88,92,75)
 )
-df
+#extracting data from a row
+df[1,]
+#extracting data from different rows
+df[c(1,3),]
+#extracting by specifying the column name
+df$Score
+#extract and filter row based on a certain condition
+df[df$Age < 25, ]
+#extract specific columns using indexing
+df2<-df[,c("Name", "Gender")]
+df2
